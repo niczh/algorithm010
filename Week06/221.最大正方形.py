6ef7661840e0813b7c2aa37145a48c2821d4b9b1
张赟 <NicZ_NS@zhangyundeMacBook-Pro.local>
2020-07-20 00:07:20 +0800

    week06 总结+code

---
+++ Week06/221.最大正方形.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=221 lang=python3
+#
+# [221] 最大正方形
+#
+
+# @lc code=start
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0: return 0
+        maxLen = 0
+        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    maxLen = max(maxLen, dp[i][j])
+        
+        res = maxLen * maxLen
+        return res
+
+# @lc code=end
+
